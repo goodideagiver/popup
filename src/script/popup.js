ede5a5e21d38193ca5783067aaa002316de419bf
Karol <kariurek@gmail.com>
2022-02-14 11:32:56 +0100

    added close backdrop and basic styles

--- src/script/popup.js
+++ src/script/popup.js
@@ -1,11 +1,22 @@
-const createBackdrop = () => {
-	const backdrop = document.createElement('section');
-	backdrop.classList.add('popupjs-backdrop');
-	return backdrop;
+const buttonClass = {
+	default: 'popup-button-default',
+	danger: 'popup-button-danger',
 };
 
 const closeModal = element => {
 	element.closest('.popupjs-backdrop').remove();
+};
+
+const createBackdrop = () => {
+	const backdrop = document.createElement('section');
+	backdrop.classList.add('popupjs-backdrop');
+	backdrop.addEventListener('click', e => {
+		if (e.target !== backdrop) {
+			return;
+		}
+		closeModal(backdrop);
+	});
+	return backdrop;
 };
 
 const getButton = (innerHTML, callback, closeOnClick = false) => {
@@ -25,7 +36,28 @@ const generateButtons = objArr => {
 	});
 };
 
+const getButtonSection = () => {
+	const buttonSection = document.createElement('section');
+	buttonSection.classList.add('popup-buttons');
+	return buttonSection;
+};
+
+const getDefaultModal = () => {
+	const button = getButton('Ok');
+	const btnSection = getButtonSection();
+	const modal = document.createElement('div');
+	modal.classList.add('modal-middle');
+	btnSection.append(button);
+	modal.append(btnSection);
+	const backdrop = createBackdrop();
+	backdrop.append(modal);
+	return backdrop;
+};
+
 const showModal = (titleText, bottomText, options) => {
 	if (options) {
+	} else {
+		const modal = getDefaultModal();
+		document.body.append(modal);
 	}
 };
