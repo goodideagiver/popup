0b900e21c68e215aaf59ab6fcd6b187959abba62
Karol <kariurek@gmail.com>
2022-02-14 14:52:03 +0100

    base

--- src/script/app.js
+++ src/script/app.js
@@ -2,4 +2,9 @@ const buttonTop = document.getElementById('top');
 const buttonMiddle = document.getElementById('middle');
 const buttonBottom = document.getElementById('bottom');
 
-buttonMiddle.addEventListener('click', () => showModal('Hello', 'World'));
+buttonMiddle.addEventListener('click', () =>
+	showModal(
+		'Zgoda na pliki cookie i dane',
+		`Na tej stronie używamy plików cookie i porównywalnych funkcji do przetwarzania informacji o urządzeniach końcowych i danych osobowych. Przetwarzanie ma na celu integracji treści, usług zewnętrznych i elementów podmiotów trzecich, analizie statystycznej/pomiarowej, spersonalizowanej reklamie oraz integracji mediów społecznościowych. W zależności od funkcji dane są przekazywane podmiotom trzecim i przez nie przetwarzane. Zgoda ta jest dobrowolna, nie jest wymagana do korzystania z naszej strony internetowej i można ją w każdej chwili odwołać za pomocą ikony w lewym dolnym rogu.`
+	)
+);
