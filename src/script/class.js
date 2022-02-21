908a9260d37228d3bc10e8fbcbaae3a4a725b87e
Karol <kariurek@gmail.com>
2022-02-21 15:15:38 +0100

    class structure part 1

--- src/script/class.js
+++ src/script/class.js
@@ -1,8 +1,37 @@
 class Style {}
-class Component {}
+
+class Attribute {
+	constructor(id, value) {
+		this.attrId = id;
+		this.value = value;
+	}
+}
+class Component {
+	constructor(text, cssClass, [id, value]) {}
+}
 class Animation {}
-class Backdrop {}
-class Button {}
+class Backdrop extends Component {
+	constructor(closeOnClick = true, ClickThrough = false) {
+		this.closeOnClick = closeOnClick;
+		this.clickThrough = this.clickThrough;
+	}
+
+	getBackdrop() {}
+}
+class Button extends Component {
+	constructor(text = 'Ok', closeOnClick = true, callbackFunc = false) {
+		this.getButton();
+	}
+
+	getButton() {}
+}
 class Popup {}
 class Position {}
-class Popup {}
+class Popup {
+	constructor(title = 'Default title', subText = false, options) {
+		//{buttons,backdropOptions,position,animation,[css classes]}} - options
+		this.render();
+	}
+
+	render() {}
+}
